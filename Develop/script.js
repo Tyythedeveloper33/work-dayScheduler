de104c86e932000c4ea9bb8609148a1cc28f5475
unknown <kingwebbe@gmail.com>
2023-07-05 21:37:00 -0400

    put index outside folder

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+
 
 //get current hour using Day.js in 12-hour format
 var currentHour = dayjs().format("H");
@@ -56,23 +34,7 @@ var dayWeek = dayjs().format('dddd M/D/YY');
 document.getElementById("currentDay").textContent = "Today is " + dayWeek;
 document.getElementById("colorKey").textContent = 'Grey = Past-Time, Red = Current-Time, Green = Future-Time';
  
-//loop through each time block
-//$(".time-block").each(function() {
-// extract the hour from the timeblocks id
-//var blockHour = parseInt($(this).attr("id").split("-")[1]);
-//console.log(blockHour)
-// compare the block hour with the current hour
-//if(blockHour < currentHour) {
-  //past time block
-  //$(this).removeClass("present", "future").addClass("past");
-//} else if(blockHour === currentHour) {
-  //current time block
- // $(this).removeClass("past", "future").addClass("present");
-//} else{
-  //future time block
- // $(this).removeClass("past", "present").addClass("future");
-//}
-//});
+
 
 //function to save the user input to local storage
 function saveToLocalStorage(){
